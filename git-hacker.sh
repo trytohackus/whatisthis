0f040832419c1099eff9a26d54dbffd35884e868
z3nth10n <ikillnukes1@gmail.com>
2018-05-03 21:57:48 +0200

    Changes

--- git-hacker.sh
+++ git-hacker.sh
@@ -13,15 +13,8 @@
 #   /_/   \_\ .__/| .__/     \_/ \__,_|_|  |___/
 #           |_|   |_|                           
 
-def_opts[0]="uploadmode"
-def_opts[1]="copymode"
-def_opts[2]="uploadurl"
-def_opts[3]="copyfolder"
-
-def_value[uploadmode]="0"
-def_value[copymode]="0"
-def_value[uploadurl]=""
-def_value[copyfolder]=""
+declare -a def_opts=("uploadmode" "copymode" "uploadurl" "copyfolder")
+declare -A values=( [uploadmode]="0" [copymode]="0" [uploadurl]="" [copyfolder]="" )
 
 #       _                  __  __       _       
 #      / \   _ __  _ __   |  \/  | __ _(_)_ __  
@@ -30,16 +23,23 @@ def_value[copyfolder]=""
 #   /_/   \_\ .__/| .__/  |_|  |_|\__,_|_|_| |_|
 #           |_|   |_|                           
 
-# First, we have to check if githack file doesn't exists to create default config.
 
 function main()
 {
+	# First, we have to check if githack file doesn't exists to create default config.
+
 	if [ ! -f githack ]; then
 		create_def_config
 	fi
-}
 
-# Second, we have to make this file be ignored by the uploader.
+	# Second, we have to make this file be ignored by the uploader.
+
+	ignore_this_file true
+
+	# Third, we have to load values
+
+	load_values
+}
 
 #       _                  _____                     
 #      / \   _ __  _ __   |  ___|   _ _ __   ___ ___ 
@@ -48,13 +48,63 @@ function main()
 #   /_/   \_\ .__/| .__/  |_|   \__,_|_| |_|\___|___/
 #           |_|   |_|                               
 
-function create_def_config {
+function create_def_config
+{
+
+	echo "[main]" > githack
 
 	for i in "${def_opts[@]}"
 	do
-		echo "${def_value[${def_opts[$i]}]}"
+		echo "  $i = ${values[$i]}" >> githack
 	done
 
+}
+
+# $1 = [bool] IsDebug?
+function ignore_this_file()
+{
+	line=""
+
+	if $1 ; then
+		line+="!"
+	fi
+
+	line+="git-hacker.sh"
+	filename=".gitignore"
+
+	if [ ! -f $filename ]; then
+		touch $filename
+		echo "$line" > $filename
+	else
+		if ! grep -Fxq "$line" $filename ; then
+			echo "$line" >> $filename
+		fi
+	fi
+}
+
+function load_values
+{
+	cfg_parser githack
+
+	cfg_section_main
+
+	for i in "${def_opts[@]}"; do
+		# Default values
+
+		#echo "Key: $i"
+		#echo "Value: ${values[$i]}"
+		eval "value=\$$i"
+		values[$i]=$value
+	done
+
+	#Checking values
+
+	#echo "----"
+
+	#for j in "${!values[@]}"; do
+	#	echo "Key: $j"
+	#	echo "Value: ${values[$j]}"
+	#done
 }
 
 #    ___ _   _ ___   ____
